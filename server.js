afcfe37930c05402455c438d7d1e8dbb6528cf1a
educornielis-droid <educornielis@gmail.com>
2026-05-13 04:27:14 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
-const app  = require('./app');
-const pool = require('./db');
+const app = require('./src/app');
+const pool = require('./src/db');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
